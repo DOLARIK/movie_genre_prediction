e819948177be8de27573a8f72d9d61cfbb0e5122
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2020-10-11 22:09:06 +0530

    Fix rel path for loading

--- app/plot_summary_genre/config.py
+++ app/plot_summary_genre/config.py
@@ -1,9 +1,11 @@
 import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # 'http://localhost:8501/v1/models/bert:predict'
 API_URI = os.environ['API_URI']
 
-INDICES_GENRE_MAPPING_LOCATION = './plot_summary_genre/cog/bert/2/indices_genre.json'
+INDICES_GENRE_MAPPING_LOCATION = os.path.join(dir_path, 'cog/bert/2/indices_genre.json')
 MAX_LENGTH = 200
 PADDING = 'max_length'
 TRUNCATION = True
