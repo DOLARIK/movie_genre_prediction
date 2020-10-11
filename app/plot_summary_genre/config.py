9e657c8b29a92cbea41bb4feaeb2e648ad34e1db
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2020-10-11 20:38:24 +0530

    Update API_URI retrieval to ENV

--- app/plot_summary_genre/config.py
+++ app/plot_summary_genre/config.py
@@ -1,6 +1,9 @@
-API_URI = 'http://localhost:8501/v1/models/bert:predict'
-INDICES_GENRE_MAPPING_LOCATION = './cog/bert/2/indices_genre.json'
+import os
 
+# 'http://localhost:8501/v1/models/bert:predict'
+API_URI = os.environ['API_URI']
+
+INDICES_GENRE_MAPPING_LOCATION = './cog/bert/2/indices_genre.json'
 MAX_LENGTH = 200
 PADDING = 'max_length'
 TRUNCATION = True
