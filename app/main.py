aadd1c06657a5d49037bcf59c80373f7efb2182a
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2020-10-12 09:07:09 +0530

    Revise endpoints
    * predict_genre -> get_genre
    * test -> predict_genre

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from plot_summary_genre import (get_genre)
 
 app = Flask(__name__)
 
-@app.route("/predict_genre", methods = ['POST'])
-def predict_genre():
+@app.route("/get_genre", methods = ['POST'])
+def get_genre():
 	genres_dict = get_genre(request.get_json())
 	return jsonify(genres_dict)
 
-@app.route("/test", methods = ['POST'])
-def test():
+@app.route("/predict_genre", methods = ['POST'])
+def predict_genre():
 	input_dict = {"plot_summary": str(request.form['plot_summary'])}
 	genres_dict = get_genre(input_dict)
 	return jsonify(genres_dict)
